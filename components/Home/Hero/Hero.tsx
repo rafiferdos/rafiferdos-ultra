6ca067e1dd7bbdb5e4464ad004a5a1bb95b9d9e4
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 16:58:03 +0600

    feat: enhance HeroClient with animated text effects and improve layout structure

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroClient from './HeroClient'
 
 const Hero = () => {
   return (
-    <div className="border">
+    <div>
       <ClientResolver>
         <HeroClient />
       </ClientResolver>
--- components/Home/Hero/HeroClient.tsx
+++ components/Home/Hero/HeroClient.tsx
@@ -1,19 +1,38 @@
 'use client'
 
+import { BlurFade } from '@/components/ui/blur-fade'
 import { LineShadowText } from '@/components/ui/line-shadow-text'
+import { MorphingText } from '@/components/ui/morphing-text'
 import { useTheme } from 'next-themes'
 
 export default function HeroClient() {
   const theme = useTheme()
   const shadowColor = theme.resolvedTheme === 'dark' ? 'white' : 'black'
+  const texts = [
+    'Full Stack Developer',
+    'MERN Stack Developer',
+    'Next.js Developer',
+    'React Developer',
+    'Node.js Developer',
+    'Express.js Developer',
+    'MongoDB Developer'
+  ]
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-4">
-        Hey I'm{' '}
-        <LineShadowText className="italic" shadowColor={shadowColor}>
-          Rafi Ferdos
-        </LineShadowText>
-      </h1>
+      <BlurFade delay={0.3} inView>
+        <h1 className="text-3xl leading-none tracking-tighter text-balance sm:text-4xl md:text-5xl lg:text-7xl">
+          I'm{' '}
+          <LineShadowText
+            className="italic text-9xl font-agale"
+            shadowColor={shadowColor}
+          >
+            Rafi
+          </LineShadowText>
+        </h1>
+      </BlurFade>
+      <BlurFade delay={0.6} inView>
+        <MorphingText texts={texts} />
+      </BlurFade>
     </div>
   )
 }
