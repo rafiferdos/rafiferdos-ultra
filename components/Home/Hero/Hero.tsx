5ddbc3aec6649127c791a37e194d5dc648f7ac76
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 16:39:42 +0600

    feat: add HeroClient component for client-side rendering with themed text shadow

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,26 +1,12 @@
 import ClientResolver from '@/components/core/client-resolver'
 import { LightRays } from '@/components/ui/light-rays'
-import { LineShadowText } from '@/components/ui/line-shadow-text'
-import { useTheme } from 'next-themes'
+import HeroClient from './HeroClient'
 
 const Hero = () => {
   return (
     <div className="border">
       <ClientResolver>
-        {() => {
-          const theme = useTheme()
-          const shadowColor = theme.resolvedTheme === 'dark' ? 'white' : 'black'
-          return (
-            <div>
-              <h1 className="text-4xl font-bold mb-4">
-                Hey I'm{' '}
-                <LineShadowText className="italic" shadowColor={shadowColor}>
-                  Rafi Ferdos
-                </LineShadowText>
-              </h1>
-            </div>
-          )
-        }}
+        <HeroClient />
       </ClientResolver>
       <LightRays />
     </div>
