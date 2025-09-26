da4593190f0260d56a4a2e89947be04d5ef59988
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 16:29:03 +0600

    feat: add LineShadowText component for animated text shadow effects and integrate into Hero component; update globals.css for animation support

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,10 +1,23 @@
-import { LightRays } from "@/components/ui/light-rays"
+import { LightRays } from '@/components/ui/light-rays'
+import { LineShadowText } from '@/components/ui/line-shadow-text'
+import { useTheme } from 'next-themes'
 
 const Hero = () => {
+  const theme = useTheme()
+  const shadowColor = theme.resolvedTheme === 'dark' ? 'white' : 'black'
   return (
-    <>
+    <div className="border">
+      {/* Hero content goes here */}
+      <div>
+        <h1 className="text-4xl font-bold mb-4">
+          Hey I'm{' '}
+          <LineShadowText className="italic" shadowColor={shadowColor}>
+            Rafi Ferdos
+          </LineShadowText>
+        </h1>
+      </div>
       <LightRays />
-    </>
+    </div>
   )
 }
 
