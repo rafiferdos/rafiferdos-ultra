daf02eb103eaf12902b67f68f45a8f20754bb745
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 12:21:41 +0600

    Add SmoothCursor component and update Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
+
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <div className='text-5xl'>
-        <p>its me <span className="font-agale">Rafi Ferdos</span></p>
+    <>
+      <div className='h-screen flex items-center justify-center'>
+        <div className='text-5xl'>
+          <p>
+            its me <span className='font-agale'>Rafi Ferdos</span>
+          </p>
+        </div>
       </div>
-    </div>
+      <SmoothCursor />
+    </>
   );
 }
