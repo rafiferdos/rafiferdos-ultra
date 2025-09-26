c116d7c0cba12c3bc666e6ce4c77ffbef0974177
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 12:27:49 +0600

    refactor: remove placeholder content from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,6 @@ import { SmoothCursor } from '@/components/ui/smooth-cursor'
 export default function Home() {
   return (
     <>
-      <div className="h-screen flex items-center justify-center">
-        <div className="text-5xl">
-          <p>
-            its me <span className="font-agale">Rafi Ferdos</span>
-          </p>
-        </div>
-      </div>
       <SmoothCursor />
     </>
   )
