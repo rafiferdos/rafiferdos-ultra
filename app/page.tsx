f4443e2d85f50c2172490ae2a693f2766c39eacc
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 16:46:21 +0600

    feat: add metadata object to Home component for improved SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Hero from '@/components/Home/Hero/Hero'
 import { SmoothCursor } from '@/components/ui/smooth-cursor'
 
+const metadata = {
+  title: 'Rafi Ferdos | Portfolio',
+  description: 'Rafi Ferdos - A MERN Stack Developer specializing in building dynamic and responsive web applications. Explore my portfolio to see my projects and skills in action. Technologies including NextJS, React, Node.js, Express, and MongoDB.',
+}
+
 export default function Home() {
   return (
     <div className="min-h-screen p-5 max-w-7xl mx-auto w-11/12">
