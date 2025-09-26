0af8d4ca57dbdc51ddb00c8e3bbf507c0344733f
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 12:29:47 +0600

    feat: implement Hero component and update Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from '@/components/Home/Hero/Hero'
 import { SmoothCursor } from '@/components/ui/smooth-cursor'
 
 export default function Home() {
   return (
-    <>
+    <div className='min-h-screen p-5 max-w-7xl mx-auto w-11/12'>
+      <Hero />
       <SmoothCursor />
-    </>
+    </div>
   )
 }
