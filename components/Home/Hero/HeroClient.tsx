36aa4fdaa4618ac0f9eeef738da4bda188ac4603
Rafi Ferdos <rafiferdos@gmail.com>
2025-09-26 16:40:34 +0600

    fix: reorder import statements in HeroClient component for consistency

--- components/Home/Hero/HeroClient.tsx
+++ components/Home/Hero/HeroClient.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useTheme } from 'next-themes'
 import { LineShadowText } from '@/components/ui/line-shadow-text'
+import { useTheme } from 'next-themes'
 
 export default function HeroClient() {
   const theme = useTheme()
