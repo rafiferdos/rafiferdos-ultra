7ee0f2a7a848762cdbddb29f186addac0fc9632d
rafiferdos <rafiferdos@gmail.com>
2025-11-21 16:08:14 +0600

    fix: update SmoothScrollProvider duration for improved scrolling performance

--- components/providers/smooth-scroll-provider.tsx
+++ components/providers/smooth-scroll-provider.tsx
@@ -10,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger)
 export const SmoothScrollProvider = ({ children }: { children: ReactNode }) => {
   useLayoutEffect(() => {
     const lenis = new Lenis({
-      duration: 1.7,
+      duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       // direction: 'vertical',
       // gestureDirection: 'vertical',
